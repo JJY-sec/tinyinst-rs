2c2752e1eb7aba859f9f43e373701010213ae46b
Your Name <you@example.com>
2023-01-07 20:19:21 +0900

    win error

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
-git clone git@github.com:elbiazo/TinyInst.git
+if exist TinyInst\ (
+  echo Yes 
+) else (
+  git clone git@github.com:elbiazo/TinyInst.git
+)
 pushd TinyInst
 git submodule update --init --recursive
 popd
